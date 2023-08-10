bf271b79a08612732b610190cc208118364eda45
Guo Yunhe <i@guoyunhe.me>
2023-08-10 22:09:44 +0800

    icon and desktop file

--- linuxporn.py
+++ linuxporn.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 
-import gi
-gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk
+import gi
+gi.require_version('Gtk', '4.0')
+
 
 def on_activate(app):
     win = Gtk.ApplicationWindow(application=app)
+    win.set_title('LinuxPorn')
     btn = Gtk.Button(label="Hello, World!")
     btn.connect('clicked', lambda x: win.close())
     win.set_child(btn)
     win.present()
 
-app = Gtk.Application(application_id='org.gtk.Example')
+
+app = Gtk.Application(application_id='me.guoyunhe.LinuxPorn')
 app.connect('activate', on_activate)
 app.run(None)
