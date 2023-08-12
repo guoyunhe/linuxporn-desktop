a78a4003466cba3cb3a72d018fb5cba5c7620459
Guo Yunhe <i@guoyunhe.me>
2023-08-12 17:24:18 +0800

    quit on ctrl+c

--- qt6/app.py
+++ qt6/app.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python3
+
+import signal
 import sys
 
 from PySide6.QtGui import QGuiApplication, QIcon
 from PySide6.QtCore import QUrl
 from PySide6.QtQml import QQmlApplicationEngine
+
 
 if __name__ == "__main__":
     QGuiApplication.setApplicationName("Gallery")
@@ -15,10 +19,12 @@ if __name__ == "__main__":
     engine.load(QUrl.fromLocalFile("qt6/qml/app.qml"))
     rootObjects = engine.rootObjects()
 
+    # quit on view error
     if not rootObjects:
-        sys.exit(-1)  # quit on view error
+        sys.exit(-1)
 
-    window = rootObjects[0]
-    window.setIcon(QIcon.fromTheme('linuxporn'))
+    # quit on Ctrl+C
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+    # quit on close button
     sys.exit(app.exec())
